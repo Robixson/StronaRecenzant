eabb7ecfb900e2ec4e800d4b4d8395da17523fb7
Robert Półtorak <robikson03@gmail.com>
2025-05-14 15:26:26 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,3 +7,20 @@ const firebaseConfig = {
   appId: "1:360185289286:web:c511ad40fde501a1858847",
   measurementId: "G-DH46ZX2765"
 };
+
+const nav = document.getElementById("nav");
+onAuthStateChanged(auth, (user) => {
+  if (!nav) return;
+  nav.innerHTML = user
+    ? `
+      <a href="index.html">Strona główna</a>
+      <a href="user.html">Mój profil</a>
+      <a href="#" onclick="logout()">Wyloguj</a>
+    `
+    : `
+      <a href="index.html">Strona główna</a>
+      <a href="login.html">Zaloguj</a>
+      <a href="register.html">Rejestracja</a>
+    `;
+});
+
