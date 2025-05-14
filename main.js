89cd855a78c25ff0f2bd0d1a93d3445e0caa7f9c
Robert Półtorak <robikson03@gmail.com>
2025-05-14 15:00:26 +0200

    baza danych

--- main.js
+++ main.js
@@ -103,3 +103,13 @@ window.onload = function () {
     renderProfile();
   }
 };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCSbsC1xbXwdSvwqnvjounLvC-S1OmdICw",
+  authDomain: "recenzant-362f1.firebaseapp.com",
+  projectId: "recenzant-362f1",
+  storageBucket: "recenzant-362f1.firebasestorage.app",
+  messagingSenderId: "360185289286",
+  appId: "1:360185289286:web:c511ad40fde501a1858847",
+  measurementId: "G-DH46ZX2765"
+};
