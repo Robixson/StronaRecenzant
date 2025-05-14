817f7f4b1959985b40cd35cc315002c88caf3320
Robert Półtorak <robikson03@gmail.com>
2025-05-14 15:17:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-// Tutaj wklej konfigurację Firebase (firebaseConfig)
-// i logikę aplikacji z wcześniejszych kroków (logowanie, recenzje, itd.)
+const firebaseConfig = {
+  apiKey: "AIzaSyCSbsC1xbXwdSvwqnvjounLvC-S1OmdICw",
+  authDomain: "recenzant-362f1.firebaseapp.com",
+  projectId: "recenzant-362f1",
+  storageBucket: "recenzant-362f1.firebasestorage.app",
+  messagingSenderId: "360185289286",
+  appId: "1:360185289286:web:c511ad40fde501a1858847",
+  measurementId: "G-DH46ZX2765"
+};
