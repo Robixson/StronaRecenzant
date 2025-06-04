d4d184ca2c76b24fa08138b553c89ca36e0bdc5b
Robert Półtorak <robikson03@gmail.com>
2025-06-04 14:25:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebas
 import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  // reszta configu
+  apiKey: "AIzaSyCSbsC1xbXwdSvwqnvjounLvC-S1OmdICw",
+  authDomain: "recenzant-362f1.firebaseapp.com",
+  projectId: "recenzant-362f1",
+  storageBucket: "recenzant-362f1.firebasestorage.app",
+  messagingSenderId: "360185289286",
+  appId: "1:360185289286:web:c511ad40fde501a1858847",
+  measurementId: "G-DH46ZX2765"
 };
 
 const app = initializeApp(firebaseConfig);
